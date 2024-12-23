0b65bde0d96b6c506940fa0cdb9ad7bb1d23a9ce
ToujoursJejepourProjet <192619701+ToujoursJejepourProjet@users.noreply.github.com>
2024-12-23 17:54:22 +0100

    Update and rename function2 to function2.py

---
+++ function2.py
@@ -0,0 +1,34 @@
+#Libraries and modules 
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import pandas as pd 
+import requests 
+import ta
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from sklearn.svm import SVR
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dropout, Dense
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error
+import statsmodels.api as sm
+import statsmodels.tsa.stattools as stattools
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.stattools import kpss 
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+import statsmodels
+import datetime
+from binance.client import Client
+import time
+from arch import arch_model
+from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import StandardScaler
+
+
